3090209400781402f51ac805a7624dfdae524bfa
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-12-19 20:39:10 +0000

    add api function to find a single post

--- api/post.ts
+++ api/post.ts
@@ -6,11 +6,25 @@ const API_BASE_URL = `http://09e86bff6900.ngrok.io/api/post`
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjVmYzkzYzViMmZhODYxMjRhMzBhMzI0ZSIsImZpcnN0TmFtZSI6IkphbmUiLCJsYXN0TmFtZSI6IkRvZSIsInVzZXJuYW1lIjoiamFuZSIsImVtYWlsIjoiamFuZUB0ZXN0LmNvbSIsImF2YXRhciI6Imh0dHBzOi8vZXUudWktYXZhdGFycy5jb20vYXBpLz9uYW1lPUphbmUrRG9lJmJhY2tncm91bmQ9cmFuZG9tJmJvbGQ9dHJ1ZSZyb3VuZGVkPXRydWUiLCJsaWtlcyI6W119LCJpYXQiOjE2MDcwMjM3MDcsImV4cCI6MzAwMDAwMDAxNjA3MDIzNzAwfQ.D-7_1jBopAgfS3Agzure21M7xnVfHC_3MLNmAccE04Q'
 
 type PostApi = {
+  findOne(_id: string): Promise<PostModel>
   likeOne(_id: string): Promise<PostModel>
   findAll(): Promise<PostModel[]>
 }
 
 const PostApi: PostApi = {
+  /**
+   * Find a single post
+   * @get /api/post/:_id
+   */
+  async findOne(_id: string): Promise<PostModel> {
+    try {
+      const response  = await buildClient(token).get(`${API_BASE_URL}/${_id}`)
+      return response.data.data
+    } catch (error) {
+      throw error.response ? error.response.data.error : error.request
+    }
+  },
+
   /**
    * like a single post
    * @param { _id } string - id of the post to like
